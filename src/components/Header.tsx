e33e9e85b153b2c8a93beed251dce487511c521c
Akila Samarawickrama <akilaishere@gmail.com>
2023-12-20 15:06:16 +0530

    login ui done

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <nav>
+      <h1>ApproveMe</h1>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Requests</a>
+        </li>
+        <li>
+          <a href="#">Approved</a>
+        </li>
+        <li>
+          <a href="#">Rejected</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
